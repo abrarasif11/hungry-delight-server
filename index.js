4e0bf81e7aa318756f8063a8331608abaae7fdd6
abrarasif11 <faasif929@gmail.com>
2023-04-05 22:06:08 +0600

    make-fake-data

--- index.js
+++ index.js
@@ -7,6 +7,11 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+const foodItems = require('./Data/product.json')
+app.get("/foodItems", (req, res) => {
+    res.send(foodItems);
+});
+
 app.get('/', (req, res) => {
     res.send('Hungry Delight Server is Running')
 });
