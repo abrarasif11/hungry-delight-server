6cc54b9137bbd97f9c894aa3e0e06552e4cc96d2
abrarasif11 <faasif929@gmail.com>
2023-04-07 23:39:43 +0600

    divided-category

--- index.js
+++ index.js
@@ -8,14 +8,26 @@ app.use(cors());
 app.use(express.json());
 
 const foodItems = require('./Data/product.json')
+const allFoodItems = require('./Data/allProducts.json')
 app.get("/foodItems", (req, res) => {
     res.send(foodItems);
 });
 
-const allFoodItems = require('./Data/allProducts.json')
+// category_id //
+app.get("/foodItems/:id", (req, res) => {
+    const id = req.params.id;
+    // console.log(id);
+    console.log(foodItems);
+    const selectedItems = allFoodItems.filter((n) => n.category_id == id);
+    console.log(selectedItems);
+    res.send(selectedItems);
+});
+
 app.get("/allFoodItems", (req, res) => {
     res.send(allFoodItems)
 })
+
+
 
 app.get('/', (req, res) => {
     res.send('Hungry Delight Server is Running')
